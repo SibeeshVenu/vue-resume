c977c9ebdaffd416fcacd4f4c60814301f543385
Sibeesh <sibeesh.venu@livetilesglobal.com>
2021-03-20 21:41:16 +0100

    Added Prism editor

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <prism-editor
+      class="code-editor"
+      v-model="code"
+      :highlight="highlighter"
+      line-numbers
+    ></prism-editor>
+  </div>
+</template>
+
+<script lang="ts">
+import { PrismEditor } from "vue-prism-editor";
+import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+
+import { highlight, languages } from "prismjs/components/prism-core"
+import "prismjs/components/prism-javascript";
+
+export default {
+  components: {
+    PrismEditor,
+  },
+  data: () => ({ code: "Start creating your resume in JSON here" }),
+  methods: {
+    highligher(code: string) {
+      return highlight(code, languages.js);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+/* required class */
+  .code-editor {
+    /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+    background: #2d2d2d;
+    color: #ccc;
+
+    /* you must provide font-family font-size line-height. Example: */
+    font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+    font-size: 14px;
+    line-height: 1.5;
+    padding: 5px;
+  }
+
+  /* optional class for removing the outline */
+  .prism-editor__textarea:focus {
+    outline: none;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <CodeEditor />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import CodeEditor from "@/components/CodeEditor.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    CodeEditor,
   },
 })
 export default class Home extends Vue {}
