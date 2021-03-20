8b36efd2a2f52eb4eb5a746e8eec52e1042182ae
Sibeesh <sibeesh.venu@livetilesglobal.com>
2021-03-20 22:51:22 +0100

    adding the code editor

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,55 +1,34 @@
 <template>
-  <div class="hello">
-    <prism-editor
-      class="code-editor"
-      v-model="code"
-      :highlight="highlighter"
-      line-numbers
-    ></prism-editor>
-  </div>
+  <prism-editor class="my-editor" v-model="code" :highlight="highlighter" line-numbers></prism-editor>
 </template>
 
-<script lang="ts">
-import { PrismEditor } from "vue-prism-editor";
-import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+<script>
+  // import Prism Editor
+  import { PrismEditor } from 'vue-prism-editor';
+  import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhere
 
-// import highlighting library (you can use any library you want just return html string)
+  // import highlighting library (you can use any library you want just return html string)
+  import { highlight, languages } from 'prismjs/components/prism-core';
+  import 'prismjs/components/prism-clike';
+  import 'prismjs/components/prism-javascript';
+  import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
 
-import { highlight, languages } from "prismjs/components/prism-core"
-import "prismjs/components/prism-javascript";
-
-export default {
-  components: {
-    PrismEditor,
-  },
-  data: () => ({ code: "Start creating your resume in JSON here" }),
-  methods: {
-    highligher(code: string) {
-      return highlight(code, languages.js);
+  export default {
+    components: {
+      PrismEditor,
     },
-  },
-};
+    data: () => ({ code: 'console.log("Hello World")' }),
+    methods: {
+      highlighter(code) {
+        return highlight(code, languages.js); // languages.<insert language> to return html with markup
+      },
+    },
+  };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-
-/* required class */
-  .code-editor {
+<style>
+  /* required class */
+  .my-editor {
     /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
     background: #2d2d2d;
     color: #ccc;
@@ -65,5 +44,4 @@ a {
   .prism-editor__textarea:focus {
     outline: none;
   }
-
 </style>
